e3a1c4b2a12fc2f427f3d3a70dffc2ba1bd81efa
Akihiro Urushiara <a.urusihara@gmail.com>
2021-12-13 07:52:29 +0900

    sample implimentation.

--- Example/UIPiPView/ViewController.swift
+++ Example/UIPiPView/ViewController.swift
@@ -7,18 +7,55 @@
 //
 
 import UIKit
+import UIPiPView
 
 class ViewController: UIViewController {
 
+    private let pipView = UIPiPView()
+    private let startButton = UIButton()
+    private let timeLabel = UILabel()
+    private var timer: Timer!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        /// Start Button
+        let margin = ((self.view.bounds.width - 200) / 2)
+        startButton.frame = .init(x: margin, y: 80, width: 200, height: 30)
+        startButton.addTarget(self, action: #selector(ViewController.toggle), for: .touchUpInside)
+        startButton.setTitle("Start PiP", for: .normal)
+        startButton.setTitleColor(.black, for: .normal)
+        startButton.backgroundColor = .white
+        startButton.layer.cornerRadius = 20
+        self.view.addSubview(startButton)
+
+        /// PiP View
+        pipView.frame = .init(x: margin, y: 160, width: 200, height: 40)
+        self.view.addSubview(pipView)
+
+        /// Time Label on PiPView
+        timeLabel.frame = .init(x: 0, y: 0, width: 200, height: 40)
+        pipView.addSubview(timeLabel)
+
+        /// Time Label  shows now.
+        timer = Timer(timeInterval: (0.1 / 60.0), repeats: true) { [weak self] _ in
+            guard let self = self else { return }
+            self.timeLabel.text = "\(Date())"
+        }
+        RunLoop.main.add(timer, forMode: .default)
+    }
+
+    @objc
+    func toggle() {
+        if (pipView.isPictureInPictureActive()) {
+            pipView.startPictureInPicture(withRefreshInterval: (0.1 / 60.0))
+        } else {
+            pipView.stopPictureInPicture()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 }
 
